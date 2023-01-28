82f6ce5d5749b25ba485f0bca5e49e76d2813d53
ChrisCluney <ccluney@gmail.com>
2023-01-27 21:56:38 -0600

    completed counter app

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let count = 0;
 
 let countEl = document.getElementById("count-el");
+
+let saveEl = document.getElementById("save-el");
+
 console.log(countEl);
 // intialize the count as 0
 // listen for clicks on the increment button
@@ -8,7 +11,15 @@ console.log(countEl);
 // change the count-el in the HTML to reflect the new count
 
 function increment() {
-  count = count + 1;
+  count += 1;
   console.log(count);
-  countEl.innerText = count;
+  countEl.textContent = count;
+}
+
+function save() {
+  let saveCount = " " + count + " - ";
+
+  saveEl.textContent += saveCount;
+  countEl.textContent = 0;
+  count = 0;
 }
